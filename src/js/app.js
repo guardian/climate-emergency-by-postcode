7e1b46e434c41801ef36018ce3db5dd22a973da7
jerseywonder <work@andyball.info>
2019-09-20 18:07:48 +1000

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,13 @@ var app = {
 
 	dataload: (key) => {
 
-		//Promise.all([loadJson(`${key[0]}?t=${new Date().getTime()}`),loadJson(`${key[1]}?t=${new Date().getTime()}`)])
-
 		loadJson(`${key}`)
 			.then((results) =>  {
 
 				var postcodes = results.sheets.postcodes
 
 				var citizen = new Climitizer(postcodes)
-				
+
 			});
 
 	}
@@ -22,7 +20,3 @@ var app = {
 }
 
 app.dataload("https://interactive.guim.co.uk/docsdata/1bClr8buuWUaKj01NolwaJy2JR_SR5hKEAjQoJPaGKcw.json");
-
-
-
-//"https://interactive.guim.co.uk/docsdata/1_oXrBEXimXW6pe5Z9mbCE7SOKH6iBCwvhWTAsjjW0x0.json"]
